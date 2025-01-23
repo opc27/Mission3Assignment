47e046ec6d8c5996040b4bb89625f6fa82d66c9a
Olivia Christensen <oliviapc09@gmail.com>
2025-01-23 11:37:23 -0700

    all code

--- FoodItem.cs
+++ FoodItem.cs
@@ -2,5 +2,31 @@ namespace Mission3Assignment;
 
 public class FoodItem
 {
+    // storing the properties of added food items
+    public string Name {get; set;}
+    public string Category {get; set;}
+    public int Quantity {get; set;}
+    public DateTime ExpirationDate {get; set;}
     
+    // constructor for the added food items
+    public FoodItem(string name, string category, int quantity, DateTime expirationDate)
+    {
+        // Make sure quantity is not negative
+        if (quantity < 0)
+        {
+            Console.WriteLine("Please enter a positive number for quantity. Setting quantity to 0.");
+            quantity = 0; // Set default to 0 if quantity is negative
+        }
+        
+        Name = name;
+        Category = category;
+        Quantity = quantity;
+        ExpirationDate = expirationDate;
+    }
+    
+    // override tostring to display food item details
+    public override string ToString()
+    {
+        return $"Name: {Name}, Category: {Category}, Quantity: {Quantity}, Expires: {ExpirationDate}";
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,100 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Olivia Christensen
+// 1/22/25
+// Mission #3 Assignment
+
+using System.ComponentModel.Design;
+using Mission3Assignment;
+
+Console.WriteLine("Welcome to the Food Bank Inventory System\n");
+
+List<FoodItem> inventory = new List<FoodItem>();
+
+int check = 1;
+
+// while loop to ensure if invalid number is put in that the user is asked again
+while (check == 1)
+{
+    // Ask the user what they would like to do
+    Console.WriteLine(
+        "Please enter a 1 if you would like to add food items.\nEnter a 2 if you would like to delete food items" +
+        "\nEnter a 3 if you would like to print a list of current food items" +
+        "\nEnter a 4 if you would like to exit the program");
+    
+    // store user input
+    int userInput = int.Parse(Console.ReadLine());
+    
+    // if statement to determine what to do with user input
+    if (userInput == 1)
+    {
+        // add food item
+        Console.Write("Enter food item name: ");
+        string name = Console.ReadLine();
+        
+        Console.Write("Enter food item category: ");
+        string category = Console.ReadLine();
+        
+        Console.Write("Enter food item quantity: ");
+        int quantity = int.Parse(Console.ReadLine());
+        
+        Console.Write("Enter expiration date (mm/dd/yyyy): ");
+        DateTime expirationDate = DateTime.Parse(Console.ReadLine());
+        
+        FoodItem newItem = new FoodItem(name, category, quantity, expirationDate);
+        inventory.Add(newItem);
+        
+        Console.WriteLine("Food Item added!\n");
+    }
+    else if (userInput == 2)
+    {
+        // delete item
+        Console.Write("Enter the name of the food item to delete: ");
+        string name = Console.ReadLine();
+        
+        // find the food item
+        FoodItem itemRemove = inventory.Find(item => item.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
+        
+        if (itemRemove != null)
+        {
+            inventory.Remove(itemRemove);
+            Console.WriteLine("Food Item removed!\n");
+        }
+        else
+        {
+            Console.WriteLine("Food item not found\n");
+        }
+    } 
+    else if (userInput == 3)
+    {
+        // print out list of food items
+        // check to make sure there are items in the inventory
+        if (inventory.Count == 0)
+        {
+            Console.WriteLine("There are no items in the inventory.\n");
+        }
+        else
+        {
+            Console.WriteLine("\nCurrent Food Items:\n");
+            foreach (FoodItem item in inventory)
+            {
+                Console.WriteLine(item + "\n");
+            }
+        }
+        
+    }
+    else if (userInput == 4)
+    {
+        // get out of loop 
+        check = 2;
+        
+        // exit program
+        Console.WriteLine("Exiting program. Goodbye!\n");
+    }
+    else
+    {
+        Console.Write("Please enter a valid number\n\n");
+        //userInput = int.Parse(Console.ReadLine()); // Re-prompt for input
+
+    }  
+}
+
+
